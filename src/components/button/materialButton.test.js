701e90ed41ca405a0e2e759030f4d535f704b826
vandelay87 <akliaissat@outlook.com>
2020-02-24 12:48:41 +0000

    button unit tests

---
+++ src/components/button/materialButton.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import MaterialButton from './materialButton';
+import { basicButton, raisedButton } from './materialButton.stories';
+
+describe('figureImage.js', () => {
+  it('renders component', () => {
+    const basicButtonTree = renderer
+      .create(basicButton())
+      .toJSON();
+    const raisedButtonTree = renderer
+      .create(raisedButton())
+      .toJSON();
+
+    expect(basicButtonTree).toMatchSnapshot();
+    expect(raisedButtonTree).toMatchSnapshot();
+  });
+
+  it('calls function when clicked', () => {
+    const mockFn = jest.fn();
+    const wrapper = shallow(<MaterialButton value="click" click={mockFn} />);
+
+    wrapper.simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
